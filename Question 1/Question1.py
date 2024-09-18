13c44636198ec65878a2a37e7f18a00b697219ac
Alex.T <alex.tarrant96@gmail.com>
2024-09-18 21:17:25 +0930

    Various updates to the code. Task 1.3.1 was rewritten as the original code disappeared.

--- Question 1/Question1.py
+++ Question 1/Question1.py
@@ -6,7 +6,7 @@
 """----- Question 1 ----""" 
 """----- Task 1.1 -----"""
 
-writefile = open("HIT137-Assignment-2\Question 1\Combined_text_file.txt", 'w')
+writefile = open("Combined_text_file.txt", 'w')
 
 #This function opens a file containing text which is provided by the parameter when the funtiton is called, and then copies the text into a new txt file named Combined_Text_File.
 def text_extract(filename):
@@ -26,17 +26,19 @@ text_extract("CSV4.csv")
 writefile.close        
 print ("Complete")
 
-"""----- Task 1.2 -----"""
+"""----- Task 1.2 -----""" 
 #Ensure the following commands are exectued in the command line or terminal to install the libraries
+"""
 pip install scispacy
 pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_core_sci_sm-0.5.4.tar.gz
 pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.5.4/en_ner_bc5cdr_md-0.5.4.tar.gz
 pip install transformers
 git clone https://github.com/dmis-lab/biobert.git
 cd biobert; pip install -r requirements.txt
+"""
 
 """----- Task 1.3.1 -----"""
-
+"""
 readfile = open("HIT137-Assignment-2\Question 1\Combined_text_file.txtCombined_text_file.txt", 'r')
 sum1=0
 worddict = {}
